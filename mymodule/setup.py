df3d5339361f1399c53e21422541fd085aeda8db
kuroko <731322845@qq.com>
2022-01-29 23:02:36 +0000

    package_temp

---
+++ mymodule/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="myp2",
+    version="5.2.0",
+    author="Kuroko",
+    author_email="kuroko@qq.com",
+    description="",
+    long_description="test Module",
+    long_description_content_type="text",
+    url="http://baidu.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ test.py
@@ -0,0 +1,10 @@
+from mypackage.package1 import p1
+
+if __name__ == "__main__":
+    # t_default = 6.388216104
+    # start_time = timeit.default_timer()
+    # myp2.bench_pidigits(ndigits=100, loops=10)
+    # elapsed_time = timeit.default_timer() - start_time
+    # print("relative elapsed: ", elapsed_time/t_default)
+    # print("afsd")
+    print(p1.os_v())
